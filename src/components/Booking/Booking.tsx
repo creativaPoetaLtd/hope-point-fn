ce19b334407980907defc5a6d575459307ddbca0
abdulkhaliq <kananuraabdulkhaliq59@gmail.com>
2024-01-24 18:20:37 +0200

    Add Booking page

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,10 +1,132 @@
-
+import Logo from "../../assets/img/Logo.svg"
+import { Link } from "react-router-dom";
 const Booking = () => {
     return (
         <>
-            <h1>
-                This is Booking page
-            </h1>
+
+            <div className="flex items-center justify-center p-12">
+
+                <div className="mx-auto w-full max-w-[550px]">
+                    <Link to="/">
+                        <img src={Logo} className=" w-13 h-18 mx-auto mb-6" alt="" />
+                    </Link>
+                    <h1 className="desktop:text-3xl tablet:text-3xl phone:text-xl mb-3 text-center">Book your available time</h1>
+                    <p className="mb-8 font-light text-center">Fill in this Form</p>
+                    <form action="" method="POST">
+                        <div className="-mx-3 flex flex-wrap">
+                            <div className="w-full px-3 sm:w-1/2">
+                                <div className="mb-5">
+                                    <label
+                                        className="mb-3 block text-base font-medium text-[#07074D]"
+                                    >
+                                        First Name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="fName"
+                                        id="fName"
+                                        placeholder="First Name"
+                                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-full px-3 sm:w-1/2">
+                                <div className="mb-5">
+                                    <label
+
+                                        className="mb-3 block text-base font-medium text-[#07074D]"
+                                    >
+                                        Last Name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="lName"
+                                        id="lName"
+                                        placeholder="Last Name"
+                                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-full px-3 sm:w-1/2">
+                                <div className="mb-5">
+                                    <label
+
+                                        className="mb-3 block text-base font-medium text-[#07074D]"
+                                    >
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name=""
+                                        id=""
+                                        placeholder="Email"
+                                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-full px-3 sm:w-1/2">
+                                <div className="mb-5">
+                                    <label
+
+                                        className="mb-3 block text-base font-medium text-[#07074D]"
+                                    >
+                                        Phone Number
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name=""
+                                        id=""
+                                        placeholder="Phone Number"
+                                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="-mx-3 flex flex-wrap">
+                            <div className="w-full px-3 sm:w-1/2">
+                                <div className="mb-5">
+                                    <label
+
+                                        className="mb-3 block text-base font-medium text-[#07074D]"
+                                    >
+                                        Date
+                                    </label>
+                                    <input
+                                        type="date"
+                                        name="date"
+                                        id="date"
+                                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-full px-3 sm:w-1/2">
+                                <div className="mb-5">
+                                    <label
+
+                                        className="mb-3 block text-base font-medium text-[#07074D]"
+                                    >
+                                        Time
+                                    </label>
+                                    <input
+                                        type="time"
+                                        name="time"
+                                        id="time"
+                                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                className="w-full hover:shadow-form rounded-md bg-[#FF3230] py-3 px-8 text-center text-base font-semibold text-white outline-none"
+                            >
+                                Book Now
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     );
 };
